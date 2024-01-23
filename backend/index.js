60a8ac44cc581912e8afc6e2a63c54b248dc1766
Musayyab Ali <MusayyabAli@crunchcraft.com>
2024-01-23 19:11:23 +0500

    implement the middleware and function to handle the error

--- backend/index.js
+++ backend/index.js
@@ -24,3 +24,14 @@ app.use("/api/auth", authRouter);
 app.listen(3000, () => {
   console.log("server is running on port 3000..!");
 });
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
