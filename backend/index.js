993e6cd3e90ae5b7420e8102cd5f38b3293ec4a4
Musayyab Ali <MusayyabAli@crunchcraft.com>
2024-01-23 15:50:09 +0500

    implement the database with mongo

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONOGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("connected with monogo db..!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
