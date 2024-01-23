f80d83c5f340f4f4d0dcc1fcf2b4c2c81761d736
Musayyab Ali <MusayyabAli@crunchcraft.com>
2024-01-23 18:25:49 +0500

    test the api router

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONOGO_CONNECTION_STRING)
   .then(() => {
-    console.log("connected with monogo db..!!");
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
-    console.log(err);
+    console.error("Error connecting to MongoDB:", err.message);
   });
 
 const app = express();
+
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
   console.log("server is running on port 3000..!");
